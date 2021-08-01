f61b287ffa561aebc1ecb0593bf3f333cf38768b
Антошка <anton_feller@mail.ru>
2021-08-01 16:52:57 +0300

    lesson_1

--- lessons/src/main/java/andersen/anton/lesson_1/HomeWorkApp.java
+++ lessons/src/main/java/andersen/anton/lesson_1/HomeWorkApp.java
@@ -1,4 +1,48 @@
 package andersen.anton.lesson_1;
 
 public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    public static void checkSumSign(){
+        int a=5;
+        int b=-10;
+        if (a+b>=0){
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void printColor(){
+        int value=112;
+        if (value<=0){
+            System.out.println("Красный");
+        }
+        else if (value>0 && value<=100){
+            System.out.println("Жёлтый");
+        }
+        else {
+            System.out.println("Зелёный");
+        }
+
+    }
+    public static void compareNumbers(){
+        int a=-122;
+        int b=37;
+        if (a>=b){
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
+    }
 }
